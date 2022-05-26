04650317012a9d08967b1353f971a02e82e39dc5
mheyen <malte.heyen@posteo.de>
2022-05-26 10:47:43 +0200

    added new dating_fitness function

--- Exercise/dino_dating.py
+++ Exercise/dino_dating.py
@@ -5,11 +5,10 @@ def dating_fitness(trex: list) -> float:
     :param trex: A vector representing a candidate solution for a T-Rex
     :return: float, dating fitness value
     """
-    [brain_size, teeth_size, height, weight, camouflage_level, claw_size, aggression] = trex
-    if weight == 0:
-        return 0
-    value = (brain_size * 200 * 1-(100/(aggression+1))) + \
-            (teeth_size + claw_size)/(teeth_size-claw_size + 1) + \
-            (height / weight) - \
-            (75 - camouflage_level)
-    return value
+    # [brain_size, teeth_size, height, weight, camouflage_level, claw_size, aggression] = trex
+    goals = [100, 30, 8, 5, 42, 7, 0]
+    value = 0
+    for x, goal in zip(trex, goals):
+        value += 100 / (1 + ((goal-x)**2))
+
+    return value / len(trex)
